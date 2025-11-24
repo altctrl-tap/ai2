d2d278639718e603d0e6717ce4a4aac6c9a4cabe
altctrl-tap <h098s02524@chunchon.gwe.hs.kr>
2025-11-24 09:28:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,10 +74,10 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
-    labels[0] : {"texts" : ["중국식 냉면은 비인기지만 맛있어"], "images" : ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.unileverfoodsolutions.co.kr%2Frecipe%2F%25EC%25A4%2591%25EA%25B5%25AD-%25EB%2583%2589%25EB%25A9%25B4-R9008730.html&psig=AOvVaw0gkyrl8HNDhwJeOzMqHXaW&ust=1764029403465000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKCTiPG-iZEDFQAAAAAdAAAAABAL"]},
-    labels[1] : {"texts" : ["짜장면은 맛있어"], "image" : ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.canceranswer.co.kr%2Fnews%2FarticleView.html%3Fidxno%3D2199&psig=AOvVaw3RTfrLEBYbqhVeRi2vblXY&ust=1764029447925000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIDUl4u_iZEDFQAAAAAdAAAAABAE"]},               
-    labels[2] : {"texts" : ["짬뽕은 맵게 맛있어"], "image" : ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fnamu.wiki%2Fw%2F%25EC%25A7%25AC%25EB%25BD%2595&psig=AOvVaw3VbWVMAR7ztTQJ6HYe1BLH&ust=1764029118287000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIjouem9iZEDFQAAAAAdAAAAABAE"]},
-    labels[3] : {"texts" : ["탕수육은 맛있어"], "image" : ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.10000recipe.com%2Frecipe%2F6935781%3Fsrsltid%3DAfmBOopCQWYwRJO_6WtxBqLYwPWA_I7cejJIYXjSkzDfoy6oO5Non7LJ&psig=AOvVaw0k5zj9tGz-oalwSukBRfi_&ust=1764029157117000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCICunoK-iZEDFQAAAAAdAAAAABAE"]},       
+    labels[0] : {"texts" : ["중국식 냉면은 비인기지만 맛있어"], "images" : ["https://www.esquirekorea.co.kr/resources_old/online/org_online_image/eq/71c93efd-352d-4fb4-8a98-dd1b51475442.jpg"]},
+    labels[1] : {"texts" : ["짜장면은 맛있어"], "image" : [https://img-cf.kurly.com/hdims/resize/%3E720x/quality/90/src/shop/data/goodsview/20230628/gv00000707399_1.jpg"]},               
+    labels[2] : {"texts" : ["짬뽕은 맵게 맛있어"], "image" : ["https://www.newiki.net/w/images/thumb/1/11/Jjampong.jpg/450px-Jjampong.jpg"]},
+    labels[3] : {"texts" : ["탕수육은 맛있어"], "image" : [https://recipe1.ezmember.co.kr/cache/recipe/2020/07/05/2e0e7c019f283bcc36d34cdee876d15b1.jpg"]},       
 }
 # ======================
 # 유틸
